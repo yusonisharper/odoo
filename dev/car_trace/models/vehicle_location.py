d854a501d302b4726ba0193fab73ec588e79b6b7
yusonisharper <wang757566564@gmail.com>
2024-02-22 23:36:24 -0800

    improve

--- dev/car_trace/models/vehicle_location.py
+++ dev/car_trace/models/vehicle_location.py
@@ -8,8 +8,14 @@ class vehicle_location(models.Model):
     _description = "vehicle current location"
     _order = "id desc"
 
-    name = fields.Char('location', required=True)
+    name = fields.Char('location')
     description = fields.Text('description')
     longitude = fields.Float("longitude", required=True)
     latitude = fields.Float("latitude", required=True)
     vehicle_id = fields.Many2one("vehicle.property")
+
+    @api.model
+    def create(self, vals):
+        prop = self.env['vehicle.property'].browse(vals['vehicle_id'])
+        prop.state = 'vehicle_discovered'
+        return super(vehicle_location, self).create(vals)
